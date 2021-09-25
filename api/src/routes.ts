0af27fb510e38c7c43354917bc80c9b869f5d915
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-09-25 14:51:13 +0530

    added route to register new user

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,5 +1,8 @@
 import { Express, Request, Response } from "express";
+import { registerUserHandler } from "./controller/auth.controller";
 
 export default function (app: Express) {
   app.get("/healthcheck", (Req: Request, res: Response) => res.sendStatus(200));
+
+  app.post("/api/auth/register", registerUserHandler);
 }
