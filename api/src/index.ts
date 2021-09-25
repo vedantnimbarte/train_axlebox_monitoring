5cc48caac9c5af0ce1ec6744965bddc576c93a1c
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-09-25 13:39:58 +0530

    getting port host from api config

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,13 +1,17 @@
 import "reflect-metadata";
 import * as express from "express";
 import routes from "./routes";
+import * as config from "config";
+
+const PORT = config.get("port") as number;
+const HOST = config.get("host") as string;
 
 const app: express.Express = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(3001, () => {
-  console.log(`server listening on port 3001`);
+app.listen(PORT, HOST, () => {
+  console.log(`Server listening at http://${HOST}:${PORT}`);
   routes(app);
 });
