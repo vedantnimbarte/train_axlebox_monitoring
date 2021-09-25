9c6fc15c0f92f3061fcda2b63231d4fae3baa734
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-09-25 17:27:46 +0530

    added enum for http status codes

---
+++ api/src/enum/http-status-codes.enum.ts
@@ -0,0 +1,8 @@
+enum StatusCodes {
+  OK = 200,
+  BAD_REQUEST = 400,
+  UNAUTHORIZED = 401,
+  NOT_FOUND = 404,
+}
+
+export default StatusCodes;
---
+++ api/src/lib/index.ts
@@ -0,0 +1,3 @@
+import password from "./password";
+
+export { password };
