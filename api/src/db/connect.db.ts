d6f823d29925d2cc64893e82fd8652ebbddf0ba3
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-09-25 14:07:15 +0530

    added db connect functionality

---
+++ api/src/db/connect.db.ts
@@ -0,0 +1,33 @@
+import "reflect-metadata";
+import * as config from "config";
+import { createConnection } from "typeorm";
+import logger from "../logger";
+import Users from "../entity/Users.entity";
+export default function connect() {
+  const db_host = config.get("dbHost") as string;
+  const db_user = config.get("dbUser") as string;
+  const db_password = config.get("dbPassword") as string;
+  const db_port = config.get("dbPort") as number;
+  const db_name = config.get("dbName") as string;
+
+  logger.info("Connecting to database...");
+
+  return createConnection({
+    type: "mysql",
+    host: db_host,
+    port: db_port,
+    username: db_user,
+    password: db_password,
+    database: db_name,
+    entities: [Users],
+    synchronize: true,
+    logging: false,
+  })
+    .then(() => {
+      logger.info(`Connected to database...`);
+    })
+    .catch((error) => {
+      logger.error(`[DATABASE ERROR]: ${error}`);
+      process.exit(1);
+    });
+}
