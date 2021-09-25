0b7173ac8caa93e67c179d7bc53e25063a5c8ad7
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-09-25 17:28:26 +0530

    added enum for response messages

---
+++ api/src/enum/messages.enum.ts
@@ -0,0 +1,7 @@
+enum ResponseMessage {
+  REGISTER_SUCCESS = "User registered successfully",
+  LOGIN_SUCCESS = "Login successfull",
+  UPDATE_SUCCESS = "Data updated successfully",
+  EMAIL_EXISTS = "Email already exists",
+  MOBILE_EXISTS = "Mobile number already exists",
+}
