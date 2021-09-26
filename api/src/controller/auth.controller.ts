c656b71dfd1b14483ccbcac5f04bb72e56130503
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-09-26 15:38:29 +0530

    getting successCode and message from auth service and passing it togenerate response function

--- api/src/controller/auth.controller.ts
+++ api/src/controller/auth.controller.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from "express";
 import StatusCodes from "../enum/http-status-codes.enum";
-import ResponseMessage from "../enum/messages.enum";
 import generateResponse from "../lib/response";
 import { registerUserService } from "../services/auth.service";
 
@@ -8,14 +7,9 @@ let data: string[] = new Array();
 
 export async function registerUserHandler(req: Request, res: Response) {
   try {
-    const message = await registerUserService(req.body);
-    return generateResponse(
-      res,
-      StatusCodes.OK,
-      ResponseMessage.REGISTER_SUCCESS,
-      data
-    );
+    const { successCode, message } = await registerUserService(req.body);
+    return generateResponse(successCode, res, StatusCodes.OK, message, data);
   } catch (err) {
-    return generateResponse(res, StatusCodes.BAD_REQUEST);
+    return generateResponse(false, res, StatusCodes.BAD_REQUEST);
   }
 }
