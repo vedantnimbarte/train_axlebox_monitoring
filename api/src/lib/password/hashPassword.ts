ef61f1bdee8360db047fa809302ae714fd1b45f9
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-09-26 17:44:16 +0530

    getting usersalt as a optional parameter for password hashing

--- api/src/lib/password/hashPassword.ts
+++ api/src/lib/password/hashPassword.ts
@@ -1,8 +1,12 @@
 import { pbkdf2Sync } from "crypto";
 import generateSalt from "./generateSalt";
 
-export default function hashPassword(password) {
-  let salt = generateSalt();
+export default function hashPassword(password: string, userSalt?: string) {
+  let salt: string;
+  if (userSalt === undefined || userSalt === null) {
+    salt = generateSalt();
+  }
+  salt = userSalt;
   let hash = pbkdf2Sync(password.toString(), salt, 1000, 64, "sha512").toString(
     "hex"
   );
