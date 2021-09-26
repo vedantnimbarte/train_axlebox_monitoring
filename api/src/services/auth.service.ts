bf211c6d8a341a8860c9acc880a8a4e09d1cb137
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-09-26 17:45:05 +0530

    added user login service to auth service

--- api/src/services/auth.service.ts
+++ api/src/services/auth.service.ts
@@ -1,6 +1,8 @@
 import ResponseMessage from "../enum/messages.enum";
+import { generateToken } from "../lib/jwt";
 import hashPassword from "../lib/password/hashPassword";
 import getUserId from "../lib/uid";
+import logger from "../logger";
 import {
   addUser,
   getUserByEmail,
@@ -26,4 +28,28 @@ export async function registerUserService(input) {
   Object.assign(input, { password, uuid, salt });
   await addUser(input);
   return { successCode: true, message: ResponseMessage.REGISTER_SUCCESS };
+}
+
+export async function loginUserService(input) {
+  const { email, password } = input;
+  let userData = await getUserByEmail(email);
+  let { hash } = hashPassword(password, userData.salt);
+  if (userData === undefined) {
+    return { successCode: false, message: ResponseMessage.EMAIL_NOT_EXISTS };
+  }
+  if (userData.password !== hash) {
+    return {
+      successCode: false,
+      message: ResponseMessage.PASSWORD_NOT_MATCHED,
+    };
+  }
+  delete input["password"];
+  const loginDataObject = {
+    token: generateToken({ userId: userData.uuid }),
+  };
+  return {
+    successCode: true,
+    message: ResponseMessage.LOGIN_SUCCESS,
+    data: loginDataObject,
+  };
 }
