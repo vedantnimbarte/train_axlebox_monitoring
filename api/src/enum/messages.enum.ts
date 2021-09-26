23c065c6953d4060a56457663bfa9b793b43e8a7
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-09-26 17:43:06 +0530

    message fir password not matched and email not exists to the enum

--- api/src/enum/messages.enum.ts
+++ api/src/enum/messages.enum.ts
@@ -4,6 +4,8 @@ enum ResponseMessage {
   UPDATE_SUCCESS = "Data updated successfully",
   EMAIL_EXISTS = "Email already exists",
   MOBILE_EXISTS = "Mobile number already exists",
+  EMAIL_NOT_EXISTS = "User with this email does not exists",
+  PASSWORD_NOT_MATCHED = "Password is incorrect",
 }
 
 export default ResponseMessage;
