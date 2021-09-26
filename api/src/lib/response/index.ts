44a8ba3c7643f2583938d8809fb3bf57733c03c1
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-09-26 15:21:13 +0530

    added success code to json response

--- api/src/lib/response/index.ts
+++ api/src/lib/response/index.ts
@@ -1,11 +1,16 @@
 import { Response } from "express";
 export default function generateResponse(
+  successCode: boolean,
   response: Response,
   http_code: number,
   message?: string,
   data?: string[]
 ) {
-  return response
-    .status(http_code)
-    .send(JSON.stringify({ message: message, data }));
+  return response.status(http_code).send(
+    JSON.stringify({
+      success: successCode,
+      message: message,
+      data,
+    })
+  );
 }
