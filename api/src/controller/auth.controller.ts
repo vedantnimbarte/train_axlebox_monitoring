b83a8cc57634c61a553efe26ab618c810a9593f9
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-09-26 17:42:18 +0530

    added login user handler to auth controller

--- api/src/controller/auth.controller.ts
+++ api/src/controller/auth.controller.ts
@@ -1,15 +1,42 @@
-import { Request, Response } from "express";
+import { Request, response, Response } from "express";
 import StatusCodes from "../enum/http-status-codes.enum";
 import generateResponse from "../lib/response";
-import { registerUserService } from "../services/auth.service";
+import logger from "../logger";
+import {
+  loginUserService,
+  registerUserService,
+} from "../services/auth.service";
 
-let data: string[] = new Array();
+let responseData = new Array();
 
 export async function registerUserHandler(req: Request, res: Response) {
   try {
     const { successCode, message } = await registerUserService(req.body);
-    return generateResponse(successCode, res, StatusCodes.OK, message, data);
+    return generateResponse(
+      successCode,
+      res,
+      StatusCodes.OK,
+      message,
+      responseData
+    );
   } catch (err) {
+    return generateResponse(false, res, StatusCodes.BAD_REQUEST);
+  }
+}
+
+export async function loginUserHandler(req: Request, res: Response) {
+  try {
+    const { successCode, message, data } = await loginUserService(req.body);
+    responseData.push({ ...data });
+    return generateResponse(
+      successCode,
+      res,
+      StatusCodes.OK,
+      message,
+      responseData
+    );
+  } catch (err) {
+    logger.info(err.message);
     return generateResponse(false, res, StatusCodes.BAD_REQUEST);
   }
 }
