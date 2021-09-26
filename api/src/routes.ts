697dc884aa73382d62af656c1e2f964b1be506ed
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-09-26 17:40:40 +0530

    added login route

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,8 +1,12 @@
 import { Express, Request, Response } from "express";
-import { registerUserHandler } from "./controller/auth.controller";
+import {
+  loginUserHandler,
+  registerUserHandler,
+} from "./controller/auth.controller";
 
 export default function (app: Express) {
   app.get("/healthcheck", (Req: Request, res: Response) => res.sendStatus(200));
 
   app.post("/api/auth/register", registerUserHandler);
+  app.post("/api/auth/login", loginUserHandler);
 }
