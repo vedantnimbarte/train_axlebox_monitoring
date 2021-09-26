f12fe15fead55b370e86eee47f5a4661f4a6cb66
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-09-26 15:37:33 +0530

    fixed validation bug in register user function

--- api/src/services/auth.service.ts
+++ api/src/services/auth.service.ts
@@ -1,7 +1,6 @@
 import ResponseMessage from "../enum/messages.enum";
 import hashPassword from "../lib/password/hashPassword";
 import getUserId from "../lib/uid";
-import logger from "../logger";
 import {
   addUser,
   getUserByEmail,
@@ -9,15 +8,14 @@ import {
 } from "../models/user.model";
 
 export async function registerUserService(input) {
-  let result;
   const { email, mobile_no } = input;
   let emailExists = await getUserByEmail(email);
-  let mobileExists = await getUserByMobileNo(mobile_no);
-  if (emailExists != undefined) {
-    return ResponseMessage.EMAIL_EXISTS;
+  if (emailExists !== undefined) {
+    return { successCode: false, message: ResponseMessage.EMAIL_EXISTS };
   }
-  if (mobileExists != undefined) {
-    return ResponseMessage.MOBILE_EXISTS;
+  let mobileExists = await getUserByMobileNo(mobile_no);
+  if (mobileExists !== undefined) {
+    return { successCode: false, message: ResponseMessage.MOBILE_EXISTS };
   }
 
   const hashedPassword = hashPassword(input["password"]);
@@ -27,5 +25,5 @@ export async function registerUserService(input) {
 
   Object.assign(input, { password, uuid, salt });
   await addUser(input);
-  return ResponseMessage.REGISTER_SUCCESS;
+  return { successCode: true, message: ResponseMessage.REGISTER_SUCCESS };
 }
