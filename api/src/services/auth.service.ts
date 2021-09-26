cdc34a72d079e6a42b1ea00089868b252f14f577
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-09-26 15:02:22 +0530

    added email and mobile validation to auth service for registering new user

--- api/src/services/auth.service.ts
+++ api/src/services/auth.service.ts
@@ -1,14 +1,31 @@
+import ResponseMessage from "../enum/messages.enum";
 import hashPassword from "../lib/password/hashPassword";
 import getUserId from "../lib/uid";
-import { addUser } from "../models/user.model";
+import logger from "../logger";
+import {
+  addUser,
+  getUserByEmail,
+  getUserByMobileNo,
+} from "../models/user.model";
 
 export async function registerUserService(input) {
+  let result;
+  const { email, mobile_no } = input;
+  let emailExists = await getUserByEmail(email);
+  let mobileExists = await getUserByMobileNo(mobile_no);
+  if (emailExists != undefined) {
+    return ResponseMessage.EMAIL_EXISTS;
+  }
+  if (mobileExists != undefined) {
+    return ResponseMessage.MOBILE_EXISTS;
+  }
+
   const hashedPassword = hashPassword(input["password"]);
   const uuid = getUserId();
   const password = hashedPassword.hash;
   const salt = hashedPassword.salt;
 
   Object.assign(input, { password, uuid, salt });
-  const { name } = await addUser(input);
-  return name;
+  await addUser(input);
+  return ResponseMessage.REGISTER_SUCCESS;
 }
