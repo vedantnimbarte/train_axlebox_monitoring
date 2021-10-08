09df88ab99d93e75430a463bc6d1016a191adb97
Vedant Nimbarte <nimbartevedant@gmail.com>
2021-10-08 16:14:50 +0530

    added coach temp chart component to component index

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,5 +2,6 @@ import TopBar from "./dashboard/Topbar";
 import TrainsList from "./dashboard/TrainsList";
 import TempDetails from "./dashboard/TempDetails";
 import TrainDetails from "./dashboard/TrainDetails";
+import CoachTempChart from "./dashboard/CoachTempChart";
 
-export { TopBar, TrainsList, TempDetails, TrainDetails };
+export { TopBar, TrainsList, TempDetails, TrainDetails, CoachTempChart };
