3975d4bb323c26f90324bdb5ee17dc285403d6ff
Vedant Nimbarte <nimbartevedant@gmail.com>
2021-10-08 15:34:52 +0530

    added train info card to index

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,4 +1,6 @@
 import TopBar from "./dashboard/Topbar";
 import TrainsList from "./dashboard/TrainsList";
 import TempDetails from "./dashboard/TempDetails";
-export { TopBar, TrainsList, TempDetails };
+import TrainDetails from "./dashboard/TrainDetails";
+
+export { TopBar, TrainsList, TempDetails, TrainDetails };
