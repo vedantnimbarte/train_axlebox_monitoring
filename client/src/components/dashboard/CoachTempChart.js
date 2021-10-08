3a33ed09dfee5b5235ae742d197d9778b45aab59
Vedant Nimbarte <nimbartevedant@gmail.com>
2021-10-08 16:14:24 +0530

    created coach temp chart component

---
+++ client/src/components/dashboard/CoachTempChart.js
@@ -0,0 +1,54 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import { purple } from "@mui/material/colors";
+import { Bar } from "react-chartjs-2";
+
+function CoachTempChart() {
+  const data = {
+    labels: ["C1", "C2", "C3", "C4", "C5", "C6"],
+    datasets: [
+      {
+        label: "Coach Temp Hits",
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: purple[700],
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
+  };
+
+  return (
+    <Paper
+      sx={{
+        margin: 2,
+        width: "45vw",
+        height: "55vh",
+        padding: 1,
+      }}
+    >
+      <Typography
+        variant="h5"
+        sx={{ color: purple[700], textAlign: "center", marginBottom: 1 }}
+      >
+        Coach Wise Temp.
+      </Typography>
+      <Box sx={{ padding: 2, overflow: "none" }}>
+        <Bar data={data} options={options} />
+      </Box>
+    </Paper>
+  );
+}
+
+export default CoachTempChart;
