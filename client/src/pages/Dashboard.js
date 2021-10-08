84be4eb7968c865bcc4b1856000c6d981fe52a1b
Vedant Nimbarte <nimbartevedant@gmail.com>
2021-10-08 15:35:11 +0530

    added train info card

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Box, Grid } from "@mui/material";
-import { TopBar, TrainsList, TempDetails } from "../components";
+import { TopBar, TrainsList, TempDetails, TrainDetails } from "../components";
 function Dashboard() {
   return (
     <Box container sx={{ height: "100vh" }}>
@@ -12,6 +12,7 @@ function Dashboard() {
         </Grid>
         <Grid md={2}>
           <TempDetails />
+          <TrainDetails />
         </Grid>
       </Box>
     </Box>
