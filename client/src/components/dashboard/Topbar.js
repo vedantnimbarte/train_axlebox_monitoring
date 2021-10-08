d9a10e1eca836eaecef1c2c09d6b72cc011d9bc4
Vedant Nimbarte <nimbartevedant@gmail.com>
2021-10-08 16:21:19 +0530

    changed color of appbar

--- client/src/components/dashboard/Topbar.js
+++ client/src/components/dashboard/Topbar.js
@@ -7,6 +7,7 @@ import IconButton from "@mui/material/IconButton";
 import AccountCircle from "@mui/icons-material/AccountCircle";
 import MenuItem from "@mui/material/MenuItem";
 import Menu from "@mui/material/Menu";
+import { purple } from "@mui/material/colors";
 
 export default function TopBar() {
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -21,7 +22,7 @@ export default function TopBar() {
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar position="static" sx={{ backgroundColor: purple[700] }}>
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             AxleBox Temperature Monitoring Portal
