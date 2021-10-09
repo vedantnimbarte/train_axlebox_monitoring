945568478504aabe1222a52a69fcccaa0090d69c
Vedant Nimbarte <nimbartevedant@gmail.com>
2021-10-09 16:42:12 +0530

    added train info component

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,5 +3,13 @@ import TrainsList from "./dashboard/TrainsList";
 import TempDetails from "./dashboard/TempDetails";
 import TrainDetails from "./dashboard/TrainDetails";
 import CoachTempChart from "./dashboard/CoachTempChart";
+import TrainInfoFormBar from "./dashboard/TrainInfoFormBar";
 
-export { TopBar, TrainsList, TempDetails, TrainDetails, CoachTempChart };
+export {
+  TopBar,
+  TrainsList,
+  TempDetails,
+  TrainDetails,
+  CoachTempChart,
+  TrainInfoFormBar,
+};
