1b8e68506bef153f1db0401021d66be73cf49c2d
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-10-10 16:08:38 +0530

    added theme properties

---
+++ client/src/themes/index.js
@@ -0,0 +1,13 @@
+import { grey } from "@mui/material/colors";
+
+export const COLORS = {
+  primary: "#263187",
+  white: "white",
+  gray: grey[700],
+};
+
+export const FONTSIZES = {
+  sm: 12,
+  md: 14,
+  lg: 16,
+};
