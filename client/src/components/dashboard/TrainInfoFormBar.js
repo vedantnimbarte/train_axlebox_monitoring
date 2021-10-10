b49f84329e80af1b26a74cf5edd092cc7c22c458
Vedant Nimbarte <nimbartevedant@gmail.com>
2021-10-10 09:29:42 +0530

    Added form fields

--- client/src/components/dashboard/TrainInfoFormBar.js
+++ client/src/components/dashboard/TrainInfoFormBar.js
@@ -2,13 +2,63 @@ import React from "react";
 import Box from "@mui/material/Box";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
-// import { white } from "@mui/material/colors";
+import TextField from "@mui/material/TextField";
+import Select from "@mui/material/Select";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Grid from "@mui/material/Grid";
 
 function TrainInfoFormBar() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" sx={{ backgroundColor: "white" }}>
-        <Toolbar></Toolbar>
+    <Box>
+      <AppBar position="static" sx={{ backgroundColor: "white", padding: 1 }}>
+        <Toolbar>
+          <Grid sx={{ display: "flex", width: 1 }}>
+            <TextField
+              id="outlined-basic"
+              label="Train No"
+              variant="outlined"
+              required
+            />
+            <FormControl sx={{ width: 0.1 }}>
+              <InputLabel id="demo-simple-select-label">Train Type</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                // value={age}
+                label="Age"
+                // onChange={handleChange}
+              >
+                <MenuItem value={10}>Wagon</MenuItem>
+                <MenuItem value={20}>ICF</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+              </Select>
+            </FormControl>
+            <TextField
+              id="outlined-basic"
+              label="No of locomotive"
+              variant="outlined"
+              required
+            />
+            <FormControl sx={{ width: 0.2 }}>
+              <InputLabel id="demo-simple-select-label">
+                Locomotive Type
+              </InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                // value={age}
+                label="Age"
+                // onChange={handleChange}
+              >
+                <MenuItem value={10}>Wagon</MenuItem>
+                <MenuItem value={20}>ICF</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+        </Toolbar>
       </AppBar>
     </Box>
   );
