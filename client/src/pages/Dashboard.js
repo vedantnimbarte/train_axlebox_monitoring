6c863f3e608756cf87ae5642e6927d0dd58695ed
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-10-11 15:45:29 +0530

    added top bar to dashboard and moved home components to its seperate component

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -7,6 +7,7 @@ import TabPanel from "@mui/lab/TabPanel";
 import { TopBar } from "../components";
 import { Colors } from "../themes";
 import Home from "./Home";
+import ControlPanel from "./ControlPanel";
 
 function Dashboard() {
   const [value, setValue] = React.useState("1");
@@ -28,17 +29,13 @@ function Dashboard() {
           <TabList onChange={handleChange} aria-label="navigation">
             <Tab label="Home" value="1" />
             <Tab label="Control Panel" value="2" />
-            <Tab label="Settings" value="3" />
           </TabList>
         </Box>
         <TabPanel value="1" sx={{ padding: 0 }}>
           <Home />
         </TabPanel>
         <TabPanel value="2" sx={{ padding: 0 }}>
-          Item Two
-        </TabPanel>
-        <TabPanel value="3" sx={{ padding: 0 }}>
-          Item Three
+          <ControlPanel />
         </TabPanel>
       </TabContext>
     </Box>
