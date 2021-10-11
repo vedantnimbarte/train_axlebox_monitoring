bb529beaecbe17f244f5a81b48e78006d694095a
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-10-11 12:10:55 +0530

    fizes sizing problem and applied theme data

--- client/src/components/dashboard/TempDetails/index.js
+++ client/src/components/dashboard/TempDetails/index.js
@@ -5,32 +5,40 @@ import { purple, grey } from "@mui/material/colors";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
+import { Colors, FontSizes } from "../../../themes";
 
 function TempDetails() {
   return (
     <Paper
       sx={{
         margin: 2,
-        width: "45vw",
-        height: "30vh",
+        width: "30vw",
+        height: "25vh",
         padding: 1,
       }}
     >
       <Typography
-        variant="h5"
-        sx={{ color: purple[700], textAlign: "center", marginBottom: 2 }}
+        sx={{
+          color: Colors.primary,
+          textAlign: "center",
+          marginBottom: 2,
+          fontSize: FontSizes.xl,
+        }}
       >
         Axle Box Temp. Details
       </Typography>
-      <Box sx={{ display: "flex", textAlign: "center" }}>
+      <Box sx={{ display: "flex", textAlign: "center", alignItems: "center" }}>
         <Grid md={4}>
           <Box sx={{ margin: 1 }}>
-            <Typography sx={{ fontSize: 12, fontWeight: 500 }}>
+            <Typography sx={{ fontSize: FontSizes.md, fontWeight: 500 }}>
               Max Temp
             </Typography>
             <Typography
-              variant="h3"
-              sx={{ color: purple[500], fontWeight: 800 }}
+              sx={{
+                color: purple[500],
+                fontWeight: 800,
+                fontSize: FontSizes.xxl,
+              }}
             >
               40.04
             </Typography>
@@ -38,12 +46,15 @@ function TempDetails() {
         </Grid>
         <Grid md={4}>
           <Box sx={{ margin: 1 }}>
-            <Typography sx={{ fontSize: 12, fontWeight: 500 }}>
+            <Typography sx={{ fontSize: FontSizes.md, fontWeight: 500 }}>
               Min Temp
             </Typography>
             <Typography
-              variant="h3"
-              sx={{ color: purple[500], fontWeight: 800 }}
+              sx={{
+                color: purple[500],
+                fontWeight: 800,
+                fontSize: FontSizes.xxl,
+              }}
             >
               40.04
             </Typography>
@@ -61,8 +72,26 @@ function TempDetails() {
               justifyContent: "space-between",
             }}
           >
-            <Typography sx={{ color: "white" }}>Total Warning</Typography>
-            <Card sx={{ padding: 1, margin: 1 }}>12</Card>
+            <Typography
+              sx={{
+                color: "white",
+                fontSize: FontSizes.sm,
+                fontWeight: "800",
+                letterSpacing: 1,
+              }}
+            >
+              Total Warning
+            </Typography>
+            <Card
+              sx={{
+                padding: 1,
+                margin: 1,
+                color: Colors.primary,
+                fontSize: FontSizes.sm,
+              }}
+            >
+              12
+            </Card>
           </Card>
           <Card
             sx={{
@@ -75,8 +104,26 @@ function TempDetails() {
               justifyContent: "space-between",
             }}
           >
-            <Typography sx={{ color: "white" }}>Total Alarm</Typography>
-            <Card sx={{ padding: 1, margin: 1 }}>12</Card>
+            <Typography
+              sx={{
+                color: Colors.white,
+                fontSize: FontSizes.sm,
+                fontWeight: "800",
+                letterSpacing: 1,
+              }}
+            >
+              Total Alarm
+            </Typography>
+            <Card
+              sx={{
+                padding: 1,
+                margin: 1,
+                fontSize: FontSizes.sm,
+                color: Colors.primary,
+              }}
+            >
+              12
+            </Card>
           </Card>
         </Grid>
       </Box>
