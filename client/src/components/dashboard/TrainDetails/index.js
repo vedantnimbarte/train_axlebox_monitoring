a5475b8596d191a96da75292f8c99c9f8e4e3f29
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-10-11 13:04:38 +0530

    Fixed sizing and styling

--- client/src/components/dashboard/TrainDetails/index.js
+++ client/src/components/dashboard/TrainDetails/index.js
@@ -2,60 +2,57 @@ import React from "react";
 import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import { purple } from "@mui/material/colors";
+import { Colors, FontSizes } from "../../../themes";
 
 function TrainDetails() {
+  const trainInfo = [
+    { key: 1, title: "Train Date", value: "0.00" },
+    { key: 2, title: "Train Time", value: "0.00" },
+    { key: 3, title: "Direction", value: "0.00" },
+  ];
   return (
     <Paper
       sx={{
         margin: 2,
-        width: "45vw",
-        height: "30vh",
-        padding: 1,
+        width: "30vw",
+        height: "25vh",
       }}
     >
       <Typography
-        variant="h5"
-        sx={{ color: purple[700], textAlign: "center", marginBottom: 2 }}
+        sx={{
+          color: Colors.white,
+          textAlign: "center",
+          marginBottom: 1,
+          backgroundColor: Colors.primary,
+          borderRadius: 1,
+          fontSize: FontSizes.lg,
+        }}
       >
         Train Info
       </Typography>
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "space-between",
-          padding: "10px 40px",
-        }}
-      >
-        <Typography sx={{ color: purple[700], fontWeight: 700 }}>
-          Train Date
-        </Typography>
-        <Typography sx={{ fontWeight: 700 }}>0.00</Typography>
-      </Box>
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "space-between",
-          padding: "10px 40px",
-        }}
-      >
-        <Typography sx={{ color: purple[700], fontWeight: 700 }}>
-          Train Time
-        </Typography>
-        <Typography sx={{ fontWeight: 700 }}>0.00</Typography>
-      </Box>
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "space-between",
-          padding: "10px 40px",
-        }}
-      >
-        <Typography sx={{ color: purple[700], fontWeight: 700 }}>
-          Direction
-        </Typography>
-        <Typography sx={{ fontWeight: 700 }}>Down</Typography>
-      </Box>
+      {trainInfo.map((item) => (
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            padding: "10px 40px",
+          }}
+          key={item.key}
+        >
+          <Typography
+            sx={{
+              color: Colors.primary,
+              fontWeight: 700,
+              fontSize: FontSizes.md,
+            }}
+          >
+            {item.title}
+          </Typography>
+          <Typography sx={{ fontWeight: 700, fontSize: FontSizes.md }}>
+            {item.value}
+          </Typography>
+        </Box>
+      ))}
     </Paper>
   );
 }
