f2a6400bc2de2a22209d0f898e01e30effa4dcb9
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-10-11 14:49:36 +0530

    added axle temp list component to grid

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,29 +1,46 @@
 import React from "react";
-import { Box, Grid } from "@mui/material";
-import {
-  TopBar,
-  TrainsList,
-  TempDetails,
-  TrainDetails,
-  CoachTempChart,
-  TrainInfoFormBar,
-} from "../components";
+import Box from "@mui/material/Box";
+import Tab from "@mui/material/Tab";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
+import { TopBar } from "../components";
+import { Colors } from "../themes";
+import Home from "./Home";
 
 function Dashboard() {
+  const [value, setValue] = React.useState("1");
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   return (
-    <Box>
+    <Box sx={{ width: "100%", typography: "body1" }}>
       <TopBar />
-      <TrainInfoFormBar />
-      <Grid sm={12} md={12} lg={12} sx={{ display: "flex" }}>
-        <Grid md={2.5}>
-          <TrainsList />
-        </Grid>
-        <Grid md={4}>
-          <TempDetails />
-          <TrainDetails />
-          <CoachTempChart />
-        </Grid>
-      </Grid>
+      <TabContext value={value}>
+        <Box
+          sx={{
+            borderBottom: 1,
+            borderColor: "divider",
+            backgroundColor: Colors.white,
+          }}
+        >
+          <TabList onChange={handleChange} aria-label="navigation">
+            <Tab label="Home" value="1" />
+            <Tab label="Control Panel" value="2" />
+            <Tab label="Settings" value="3" />
+          </TabList>
+        </Box>
+        <TabPanel value="1" sx={{ padding: 0 }}>
+          <Home />
+        </TabPanel>
+        <TabPanel value="2" sx={{ padding: 0 }}>
+          Item Two
+        </TabPanel>
+        <TabPanel value="3" sx={{ padding: 0 }}>
+          Item Three
+        </TabPanel>
+      </TabContext>
     </Box>
   );
 }
