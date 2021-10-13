8cb9298e4cd567c57f2fe63d89d19246937a4a37
Vedant Nimbarte <vedantnimbarte7@gmail.com>
2021-10-13 17:38:07 +0530

    added trains route to router

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -3,10 +3,12 @@ import {
   loginUserHandler,
   registerUserHandler,
 } from "./controller/auth.controller";
+import { getTrainsHandler } from "./controller/train.controller";
 
 export default function (app: Express) {
   app.get("/healthcheck", (Req: Request, res: Response) => res.sendStatus(200));
 
   app.post("/api/auth/register", registerUserHandler);
   app.post("/api/auth/login", loginUserHandler);
+  app.get("/api/trains", getTrainsHandler);
 }
